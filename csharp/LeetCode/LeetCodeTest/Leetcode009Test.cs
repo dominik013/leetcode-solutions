65c94b5ca94c2bb4c7b201008e1f0211586bbcf3
Dominik Reichinger <reichinger.dominik@gmail.com>
2020-09-21 17:27:53 +0200

    Leetcode009

---
+++ csharp/LeetCode/LeetCodeTest/Leetcode009Test.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTest
+{
+	[TestClass]
+	public class Leetcode009Test
+	{
+		[TestMethod]
+		public void Leetcode009TestOne()
+		{
+			var output = Leetcode009.IsPalindrome(121);
+			Assert.AreEqual(true, output);
+		}
+
+		[TestMethod]
+		public void Leetcode009TestTwo()
+		{
+			var output = Leetcode009.IsPalindrome(-121);
+			Assert.AreEqual(false, output);
+		}
+
+		[TestMethod]
+		public void Leetcode009TestThree()
+		{
+			var output = Leetcode009.IsPalindrome(10);
+			Assert.AreEqual(false, output);
+		}
+	}
+}
