724b32e91232a1cee0b28b14937c20c18f8b7ef3
Dominik Reichinger <reichinger.dominik@gmail.com>
2020-10-01 11:26:45 +0200

    Leetcode 13 + 14

---
+++ csharp/LeetCode/LeetCode/Leetcode014.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public class Leetcode014
+	{
+		public static string LongestCommonPrefix(string[] strs)
+		{
+			if (strs.Length == 0) return String.Empty;
+			int minLen = int.MaxValue;
+			var longestCommonPrefix = new StringBuilder();
+			bool common = true;
+
+			foreach (var s in strs)
+			{
+				minLen = Math.Min(minLen, s.Length);
+			}
+
+			for (int i = 0; i < minLen && common; i++)
+			{
+				char currentCharacter = strs[0][i];
+				for (int j = 0; j < strs.Length; j++)
+				{
+					if (currentCharacter != strs[j][i])
+					{
+						common = false;
+						break;
+					}
+				}
+				if (common) longestCommonPrefix.Append(currentCharacter);
+			}
+
+			return longestCommonPrefix.ToString();
+		}
+	}
+}
---
+++ csharp/LeetCode/LeetCodeTest/Leetcode013Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCodeTest
+{
+	 [TestClass]
+	public class Leetcode013Test
+	{
+		[TestMethod]
+		public void Leetcode013TestOne()
+		{
+			var output = Leetcode013.RomanToInt("III");
+			Assert.AreEqual(3, output);
+		}
+
+		[TestMethod]
+		public void Leetcode013TestTwo()
+		{
+			var output = Leetcode013.RomanToInt("IV");
+			Assert.AreEqual(4, output);
+		}
+
+		[TestMethod]
+		public void Leetcode013TestThree()
+		{
+			var output = Leetcode013.RomanToInt("IX");
+			Assert.AreEqual(9, output);
+		}
+		
+		[TestMethod]
+		public void Leetcode013TestFour()
+		{
+			var output = Leetcode013.RomanToInt("LVIII");
+			Assert.AreEqual(58, output);
+		}
+
+		[TestMethod]
+		public void Leetcode013TestFive()
+		{
+			var output = Leetcode013.RomanToInt("MCMXCIV");
+			Assert.AreEqual(1994, output);
+		}
+	}
+}
